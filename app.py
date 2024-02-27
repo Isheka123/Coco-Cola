f291f545ad90c0c6df1e971edd5a323fd597d008
Kavya Sai Isheka <yakkalakavyasaiisheka@gmail.com>
2024-02-27 23:11:07 +0530

    complete

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def convert_boxes(boxes):
     return boxes * np.array([width, height, width, height])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
